dfca9b796cb3d7ddcf44641f8dda0f1c8718eb52
Lauma <lauma.pretkalnina@gmail.com>
2015-05-11 15:42:00 +0300

    Refactoring.

--- src/lv/ailab/segmenter/Segmenter.java
+++ src/lv/ailab/segmenter/Segmenter.java
@@ -27,7 +27,7 @@ public class Segmenter
         lexicon = l;
     }
 
-    public Results segment(String s)
+    public SegmentationResult segment(String s)
     {
         // i-th position contains information, if there is a segmentation
         // variant found, ending in this position.
@@ -75,7 +75,7 @@ public class Segmenter
                 }
             }
         }
-        return new Results(s, memorizedWords.get(s.length()), foundWords);
+        return new SegmentationResult(s, memorizedWords.get(s.length()), foundWords);
     }
 
     /**
@@ -109,77 +109,4 @@ public class Segmenter
         out.close();
     }
 
-    /**
-     * Object for representing Segmenter's results.
-     */
-    public static class Results
-    {
-        /**
-         * Original string.
-         */
-        String original;
-        /**
-         * Fond segmentation variants for given string.
-         */
-        public ArrayList<ArrayList<String>> segmentations;
-        /**
-         * All valid "words" (accepted as word by lexicon or regexp) that are
-         * found given string.
-         */
-        public HashMap<String, List<Lexicon.Entry>> foundWords;
-
-        public Results (String original,
-                ArrayList<ArrayList<String>> segmentations,
-                HashMap<String, List<Lexicon.Entry>> foundWords)
-        {
-            this.original = original;
-            this.segmentations = segmentations;
-            this.foundWords = foundWords;
-        }
-
-        public String toJSON()
-        {
-            StringBuilder res = new StringBuilder();
-            res.append("{\n\t\"String\":\"");
-            res.append(original);
-            res.append("\",\n\t\"SegmentationVariants\":[");
-            for (ArrayList<String> variant : segmentations)
-            {
-                res.append("\n\t\t[");
-                for (String word : variant)
-                {
-                    res.append("\"");
-                    res.append(word);
-                    res.append("\", ");
-                }
-                if (res.toString().endsWith(", "))
-                    res.delete(res.length()-2, res.length());
-                res.append("],");
-            }
-            if (res.toString().endsWith(","))
-                res.delete(res.length()-1, res.length());
-            res.append("],\n\t\"FoundWords\":[");
-            for (String word : foundWords.keySet())
-            {
-                res.append("\n\t\t\"");
-                res.append(word);
-                res.append("\":[");
-                for (Lexicon.Entry info : foundWords.get(word))
-                {
-                    res.append("{\"Lemma\":\"");
-                    res.append(info.lemma);
-                    res.append("\", \"Source\":\"");
-                    res.append(info.lang);
-                    res.append("\"}, ");
-                }
-                if (res.toString().endsWith(", "))
-                    res.delete(res.length()-2, res.length());
-                res.append("],");
-            }
-            if (res.toString().endsWith(","))
-                res.delete(res.length()-1, res.length());
-            res.append("]\n}");
-            return res.toString();
-        }
-    }
 }
